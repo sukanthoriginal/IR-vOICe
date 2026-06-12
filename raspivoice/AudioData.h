d7ee62db5e8d67494b7198c36b8473ecdec5f5c3
Sukanth <sukanth.original@gmail.com>
2026-06-12 13:05:15 +0530

    Revert "Eliminate audio click by streaming PCM to a persistent aplay subprocess"
    This reverts commit 5f95cab403dc7e43315e8d91d32afc4616a3d130.

--- raspivoice/AudioData.h
+++ raspivoice/AudioData.h
@@ -16,14 +16,6 @@ private:
 	int volume;
 	int newvolume;
 
-	// Persistent aplay subprocess. Keeping the ALSA device open across
-	// frames eliminates the click that happens when aplay reopens it
-	// every 1.05s. Lazy open on first Play(), reopened on card change.
-	FILE* aplay_pipe_;
-	int pipe_card_;
-	void ensurePipeOpen();
-	void closePipe();
-
 	void wi(FILE* fp, uint16_t i);
 	void wl(FILE* fp, uint32_t l);
 	int updateVolume();
@@ -33,7 +25,6 @@ public:
 
 	static void Init();
 	AudioData(int card_number, int sample_freq_Hz = 48000, int sample_count = 0, bool use_stereo = true);
-	~AudioData();
 	
 	uint16_t *Data() { return &samplebuffer[0]; };
 
